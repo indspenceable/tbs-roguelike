180fdb7124098d21e2b8901e5f1144396a938909
Daniel Spencer <dspencer@squareup.com>
2015-07-11 14:01:18 -0700

    Starts Adding UI. kinda sucks

---
+++ Assets/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+	[SerializeField]
+	private Text UnitInfoText;
+	[SerializeField]
+	private Image CurrentTerrainImage;
+
+	private SpriteRenderer spriteRenderer;
+	void Start() {
+	}
+
+	public void CurrentTile(Tile t) {
+		CurrentTerrainImage.sprite = t.GetComponent<SpriteRenderer>().sprite;
+	}
+
+	public void CurrentUnit(Unit u) {
+	}
+
+	public void Show(bool value) {
+		gameObject.SetActive(value);
+	}
+}
